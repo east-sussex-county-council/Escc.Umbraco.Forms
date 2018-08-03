74db566f8e92ed3abe6e95cf86aecef7fc8ce65b
Rick Mason <richard.mason@eastsussex.gov.uk>
2018-08-03 17:58:23 +0100

    Add tests for the email validation method

---
+++ Escc.Umbraco.Forms.FieldTypes.Tests/EmailFieldTests.cs
@@ -0,0 +1,39 @@
+﻿using Escc.Umbraco.Forms.FieldTypes;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escc.Umbraco.Forms.FieldTypes.Tests
+{
+    [TestFixture]
+    public class EmailFieldTests
+    {
+        [TestCase("just a name")]
+        [TestCase("first.last@example.org.")]
+        [TestCase("first.last.@example.org")]
+        [TestCase("first.last@example..org")]
+        [TestCase("first.last@o'no.org")]
+        public void InvalidEmailIsRejected(string email)
+        {
+            var validator = new Email();
+
+            var isValid = validator.ValidateEmailAddress(email);
+
+            Assert.IsFalse(isValid);
+        }
+
+        [TestCase("first.last@example.org")]
+        [TestCase("first.o'yes@example.org")]
+        public void ValidEmailIsAccepted(string email)
+        {
+            var validator = new Email();
+
+            var isValid = validator.ValidateEmailAddress(email);
+
+            Assert.IsTrue(isValid);
+        }
+    }
+}
---
+++ Escc.Umbraco.Forms.FieldTypes.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Escc.Umbraco.Forms.FieldTypes.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Escc.Umbraco.Forms.FieldTypes.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b40addf-04ca-44ab-b05b-b9a374f77bca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
