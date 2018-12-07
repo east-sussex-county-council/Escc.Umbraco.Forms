66ec4b86cba726b48485e7603ef2ba9639f23b0d
Rick Mason <richard.mason@eastsussex.gov.uk>
2018-12-07 17:00:28 +0000

    Update the retention API again to move more work into the console app, as even one form with a lot of entries can be too much to process in one web request.

--- Escc.Umbraco.Forms.Workflows.ApplyRetentionSchedule/Program.cs
+++ Escc.Umbraco.Forms.Workflows.ApplyRetentionSchedule/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -15,7 +16,7 @@ namespace Escc.Umbraco.Forms.Workflows.ApplyRetentionSchedule
 {
     class Program
     {
-        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+        private static readonly ILog _log = LogManager.GetLogger(typeof(Program));
 
         static void Main(string[] args)
         {
@@ -42,42 +43,92 @@ namespace Escc.Umbraco.Forms.Workflows.ApplyRetentionSchedule
                     throw new ConfigurationErrorsException("appSettings > ApiPassword was not set in app.config");
                 }
 
-                var request = WebRequest.Create($"{baseUrl.TrimEnd('/')}/umbraco/api/UmbracoFormsRetentionApi/ListForms");
-                request.Credentials = new NetworkCredential(apiUser, apiPassword);
-                log.Info($"Requesting {request.RequestUri}");
+                var credentials = new NetworkCredential(apiUser, apiPassword);
+
+                // Make lots of separate requests because even one form with lots of entries can be too much work
+                // to complete within the timeout of one web request.
+                //
+                // Run synchronously so that we don't hit the web server with requests to process every form at once.
+
+                // Get the list of forms
+                var forms = MakeRequest<IEnumerable<Guid>>(new Uri($"{baseUrl.TrimEnd('/')}/umbraco/api/UmbracoFormsRetentionApi/ListForms"), credentials, true);
+                _log.Info($"{forms.Count()} forms found");
+
+                foreach (var formId in forms)
+                {
+                    // Get the list of entries
+                    var entries = MakeRequest<IEnumerable<Guid>>(new Uri($"{baseUrl.TrimEnd('/')}/umbraco/api/UmbracoFormsRetentionApi/ListEntries?formId={formId}"), credentials, false);
+                    if (entries == null) continue;
+                    _log.Info($"{entries.Count()} entries found for form '{formId}'");
+
+                    foreach (var entryId in entries)
+                    {
+                        // Get the retention date for the entry
+                        var retentionDate = MakeRequest<DateTime?>(new Uri($"{baseUrl.TrimEnd('/')}/umbraco/api/UmbracoFormsRetentionApi/RetentionDate?entryId={entryId}"), credentials, false);
+
+                        // then compare it to the current date and delete the record if the retention date has passed
+                        if (retentionDate.HasValue && retentionDate < DateTime.Today)
+                        {
+                            try
+                            {
+                                var deleteRequest = WebRequest.Create($"{baseUrl.TrimEnd('/')}/umbraco/api/UmbracoFormsRetentionApi/DeleteEntry?entryId={entryId}");
+                                deleteRequest.Method = "DELETE";
+                                deleteRequest.Credentials = credentials;
+                                _log.Info($"Deleting entry '{entryId}' for form '{formId}' as its retention date '{retentionDate.Value.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)}' has passed.");
+                                using (var deleteResponse = deleteRequest.GetResponse())
+                                {
+                                }
+                            }
+                            catch (WebException ex)
+                            {
+                                ex.ToExceptionless().Submit();
+                                _log.Error(ex.Message);
+                                continue;
+                            }
+                        }
+
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.ToExceptionless().Submit();
+                _log.Error(ex.Message);
+                throw;
+            }
+        }
+
+        private static T MakeRequest<T>(Uri url, NetworkCredential credential, bool throwErrors)
+        {
+            try
+            {
+                var request = WebRequest.Create(url);
+                request.Credentials = credential;
+                _log.Info($"Requesting {request.RequestUri}");
                 using (var response = request.GetResponse())
                 {
                     using (var responseStream = response.GetResponseStream())
                     {
                         using (var reader = new StreamReader(responseStream))
                         {
-                            // Get the list of forms
-                            var forms = JsonConvert.DeserializeObject<IEnumerable<Guid>>(reader.ReadToEnd());
-                            log.Info($"{forms.Count()} forms found");
-
-                            // Make a separate DELETE request for each form. This is more scaleable that doing everything in one web request,
-                            // since each web request is limited to the work necessary to process entries for one form.
-                            
-                            // Run synchronously so that we don't hit the web server with requests to process every form at once.
-                            foreach (var formId in forms)
-                            {
-                                var deleteRequest = WebRequest.Create($"{baseUrl.TrimEnd('/')}/umbraco/api/UmbracoFormsRetentionApi/ApplySetDateRetentionSchedule?formId={formId}");
-                                deleteRequest.Method = "DELETE";
-                                deleteRequest.Credentials = new NetworkCredential(apiUser, apiPassword);
-                                log.Info($"Requesting {deleteRequest.RequestUri}");
-                                using (var deleteResponse = deleteRequest.GetResponse())
-                                {
-                                }
-                            }
+                            return JsonConvert.DeserializeObject<T>(reader.ReadToEnd());
                         }
                     }
                 }
             }
             catch (Exception ex)
             {
-                ex.ToExceptionless().Submit();
-                log.Error(ex.Message);
-                throw;
+                if (throwErrors)
+                {
+                    throw;
+                }
+                else
+                {
+                    ex.ToExceptionless().Submit();
+                    _log.Error(ex.Message);
+                    return default(T);
+                }
             }
         }
     }
--- Escc.Umbraco.Forms.Workflows/UmbracoFormsRetentionApiController.cs
+++ Escc.Umbraco.Forms.Workflows/UmbracoFormsRetentionApiController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Umbraco.Core.Logging;
+using Umbraco.Forms.Core;
 using Umbraco.Forms.Data.Storage;
 using Umbraco.Web.WebApi;
 
@@ -23,53 +24,106 @@ namespace Escc.Umbraco.Forms.Workflows
         [HttpGet]
         public IEnumerable<Guid> ListForms()
         {
-            using (var formStorage = new FormStorage())
+            try
             {
-                return formStorage.GetAllForms().Select(form => form.Id);
+                using (var formStorage = new FormStorage())
+                {
+                    return formStorage.GetAllForms().Select(form => form.Id);
+                }
+            }
+            catch (Exception exception)
+            {
+                LogHelper.Error<UmbracoFormsRetentionApiController>(exception.Message, exception);
+                throw;
             }
         }
 
         /// <summary>
-        /// Applies the 'after a set date' retention schedule defined in <see cref="Workflows.RetentionAfterSetDateWorkflow"/>.
+        /// Gets a list of all entries for a form
         /// </summary>
-        [HttpDelete]
-        public void ApplySetDateRetentionSchedule(Guid formId)
+        /// <param name="formId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<Guid> ListEntries(Guid formId)
         {
             try
             {
-                LogHelper.Info<UmbracoFormsRetentionApiController>($"ApplySetDateRetentionSchedule for form {formId} started at {DateTime.UtcNow} (UTC).");
-
-                // Loop through all fields on all records looking for the retention date,
-                // then compare it to the current date and delete the record if the retention date has passed
                 using (var formStorage = new FormStorage())
                 {
                     var form = formStorage.GetForm(formId);
+                    if (form == null) return new Guid[0];
                     using (var recordStorage = new RecordStorage())
                     {
-                        var records = recordStorage.GetAllRecords(form);
-                        foreach (var record in records)
+                        return recordStorage.GetAllRecords(form, false).Select(record => record.UniqueId);
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                LogHelper.Error<UmbracoFormsRetentionApiController>(exception.Message, exception);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Gets the retention date, if any, for a given form entry
+        /// </summary>
+        /// <param name="entryId"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public DateTime? RetentionDate(Guid entryId)
+        {
+            try
+            {
+                using (var recordStorage = new RecordStorage())
+                {
+                    var record = recordStorage.GetRecordByUniqueId(entryId);
+                    if (record == null) return null;
+
+                    foreach (var field in record.RecordFields)
+                    {
+                        if (field.Value.Alias == "deleteAfter")
                         {
-                            foreach (var field in record.RecordFields)
+                            if (field.Value.HasValue() && !String.IsNullOrEmpty(field.Value.ValuesAsString()))
                             {
-                                if (field.Value.Alias == "deleteAfter" && field.Value.HasValue() && !String.IsNullOrEmpty(field.Value.ValuesAsString()))
+                                DateTime retentionDate;
+                                if (DateTime.TryParse(field.Value.ValuesAsString(), out retentionDate))
                                 {
-                                    DateTime retentionDate;
-                                    if (DateTime.TryParse(field.Value.ValuesAsString(), out retentionDate))
-                                    {
-                                        if (retentionDate < DateTime.Today)
-                                        {
-                                            LogHelper.Info<UmbracoFormsRetentionApiController>($"Deleting record '{record.UniqueId}' for form '{form.Name}' with Id '{form.Id}' as its retention date '{field.Value.ValuesAsString()}' has passed.");
-                                            recordStorage.DeleteRecord(record, form);
-                                            break;
-                                        }
-                                    }
+                                    return retentionDate;
                                 }
                             }
+                            break;
                         }
                     }
                 }
+            }
+            catch (Exception exception)
+            {
+                LogHelper.Error<UmbracoFormsRetentionApiController>(exception.Message, exception);
+                throw;
+            }
+            return null;
+        }
 
-                LogHelper.Info<UmbracoFormsRetentionApiController>($"ApplySetDateRetentionSchedule for form {formId} completed at {DateTime.UtcNow} (UTC).");
+        /// <summary>
+        /// Deletes a form entry
+        /// </summary>
+        [HttpDelete]
+        public void DeleteEntry(Guid entryId)
+        {
+            try
+            {
+                using (var recordStorage = new RecordStorage())
+                {
+                    var record = recordStorage.GetRecordByUniqueId(entryId);
+                    if (record == null) return;
+
+                    var form = record.GetForm();
+                    if (form == null) return;
+
+                    LogHelper.Info<UmbracoFormsRetentionApiController>($"Deleting record '{record.UniqueId}' for form '{form.Name}' with id '{form.Id}'.");
+                    recordStorage.DeleteRecord(record, form);
+                }
             }
             catch (Exception exception)
             {
