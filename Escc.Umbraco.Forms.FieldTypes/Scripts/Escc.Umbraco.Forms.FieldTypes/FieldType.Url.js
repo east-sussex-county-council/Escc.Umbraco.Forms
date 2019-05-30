c56bc6c5d6311c0f791adf0f18293cce3b2bfd0b
Rick Mason <richard.mason@eastsussex.gov.uk>
2019-05-30 13:44:14 +0100

    Add file missing from repository. Remove app.configs which are not required.

---
+++ Escc.Umbraco.Forms.FieldTypes/Scripts/Escc.Umbraco.Forms.FieldTypes/FieldType.Url.js
@@ -0,0 +1,14 @@
+﻿if (typeof (jQuery) !== 'undefined') {
+    "use strict";
+    $(function () {
+        // If someone enters a web address that doesn't start with https?, add the protocol so that
+        // we can use the HTML 'url' field type to trigger a helpful keyboard
+        $('.umbraco-forms-field.url input[type=text],.umbraco-forms-field.url input[type=url]').on('blur', function () {
+            var string = $(this).val();
+            if (!string.match(/^https?:/) && string.length) {
+                string = "https://" + string;
+                $(this).val(string)
+            }
+        });
+    });
+}
